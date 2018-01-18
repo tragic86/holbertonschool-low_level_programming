1e6ef8936389ec01936c35083da220c32454b118
tragic86 <oneil5@aol.com>
2018-01-18 03:07:07 +0000

    print a text with new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program to print in c using printf
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
