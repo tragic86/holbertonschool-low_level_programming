56cad3674c3416d4a5501efe6fa48397d3586317
tragic86 <oneil5@aol.com>
2018-01-18 03:01:40 +0000

    a C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print a message in c
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+
+}
