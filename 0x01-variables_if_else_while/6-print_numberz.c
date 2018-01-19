203325a858a3206e7d2c83c0e79b57c9d9881fbc
tragic86 <oneil5@aol.com>
2018-01-19 02:40:51 +0000

    print single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ *
+ * Return: none
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+
+
+
+	putchar('\n');
+	return (0);
+}
