cd16f612ee4a2e1ec46c61f552df8ed6c8f4773c
tragic86 <oneil5@aol.com>
2018-01-19 01:42:47 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - a program that print the alphbet in lowercase
+ *
+ * Return: none
+ *
+ */
+
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c != 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
