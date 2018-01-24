2f37ccf8e5f5d8ee0ee798289410d8bd032ea09d
tragic86 <oneil5@aol.com>
2018-01-24 06:25:13 +0000

    program that prints abs of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _abs - a function that computes the abs value of integer
+ *
+ * @num: number being used
+ *
+ * Return: none
+ *
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = (-1) * num;
+		return (num);
+	}
+	return (num);
+
+	return (0);
+
+
+}
