d18bc07f51deb3a457ce07b8355da1f79ac332c8
tragic86 <oneil5@aol.com>
2018-01-24 06:23:19 +0000

    program that checks for all letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks for alphabetic letters
+ *
+ * @c: letter being used
+ *
+ * Return: 1 if letter 0 if otherwise
+ */
+
+
+int _isalpha(int c)
+{
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
