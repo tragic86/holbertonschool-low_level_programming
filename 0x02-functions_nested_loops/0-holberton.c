3d30c2685c51a6d65287c620b30bc0656a28e14c
tragic86 <oneil5@aol.com>
2018-01-24 06:19:59 +0000

    program that prints holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * main - a program that prints Holberton with new line
+ *
+ *
+ *
+ * Return: none
+ *
+ */
+
+int main(void)
+{
+
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+
+
+	return (0);
+
+
+}
