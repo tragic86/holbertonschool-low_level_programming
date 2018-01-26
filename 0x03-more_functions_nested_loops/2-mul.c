3db732b4a15bb9ccfdc4653129d1f02b2d0b01d6
tragic86 <oneil5@aol.com>
2018-01-26 02:37:34 +0000

    multipy two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: none
+ */
+
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+
+}
