8a87098842d773f8cc6d177d6e707c593661d95a
tragic86 <oneil5@aol.com>
2018-01-26 02:30:54 +0000

    check for a digit

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for digit
+ *
+ * @c: digit
+ *
+ * Return: none
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+
+
+}
