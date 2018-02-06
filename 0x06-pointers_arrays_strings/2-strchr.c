781dbd068102257bed5928aeb7949beb844ca4c7
tragic86 <oneil5@aol.com>
2018-02-06 06:49:09 +0000

    find a character in a string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * _strchr - a function that finds a character in a string
+ * @s: string
+ * @c: character to be found
+ *
+ * Return: pointer to first character or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+			s++;
+		}
+	}
+
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	else
+	{
+		return ('\0');
+	}
+
+
+}
