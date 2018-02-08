880d6a5aa3d0c94c5c4e34cb7da6a60eede4573a
tragic86 <oneil5@aol.com>
2018-02-08 04:35:36 +0000

    value of x rasied to power of y

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - a function that returns x to the power of y
+ *
+ * @x: first value
+ * @y: value rasied by first
+ * Return: value of x
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
