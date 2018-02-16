d2c73fcef6843d5a2ff9d03349e818bb274cfd62
tragic86 <oneil5@aol.com>
2018-02-16 00:13:20 +0000

    function to allocate memory

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * malloc_checked - a function that allocates memory
+ *
+ * @b: memory to allocate
+ *
+ *
+ * Return: pointer to new memory
+ */
+void *malloc_checked(unsigned int b)
+{
+
+	int *string;
+
+
+
+	string = malloc(sizeof(unsigned int) * b);
+		if (string == NULL)
+		{
+			exit(98);
+		}
+		else
+		{
+			return (string);
+		}
+
+
+
+
+
+
+		return (0);
+
+
+}
