33d0c8c893fd3dd0ce5f4ac528238661bb65194d
tragic86 <oneil5@aol.com>
2018-02-19 22:59:11 +0000

    create a struct with elements

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - defines a structure for dogs
+ * @name: Dog name
+ *
+ * @age: Dog age
+ *
+ * @owner: Owner of Dog
+ *
+ * Return: none
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
