9bc70525b1a7b0c4b73474e7648407265db74f79
tragic86 <oneil5@aol.com>
2018-02-20 04:02:14 +0000

    free malloc dogs

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function that free malloc dogs
+ *
+ * @d: struct
+ *
+ *
+ * Return: None
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+
+		free(d->name);
+		free(d->owner);
+		free(d);
+
+
+
+}
