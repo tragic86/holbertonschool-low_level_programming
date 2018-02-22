7a480469728680157626702be63dec418ae864ef
tragic86 <oneil5@aol.com>
2018-02-22 00:56:50 +0000

    added header file

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef _FUNCTIONS_H
 #define _FUNCTIONS_H
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 #endif
