a8449f46aae5577c0712beb3e9907627992d879e
tragic86 <oneil5@aol.com>
2018-02-25 02:34:10 +0000

    added header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 #endif
