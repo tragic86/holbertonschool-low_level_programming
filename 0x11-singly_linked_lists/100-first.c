5f8484ab89b17d5766fcd73ace2364ede07a6a03
tragic86 <oneil5@aol.com>
2018-03-02 21:31:47 +0000

    ade a darnell function

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * darnell_ - prints message before main because darnell said so
+ *
+ *
+ * Return: none
+ */
+void __attribute__((constructor)) darnell_()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
