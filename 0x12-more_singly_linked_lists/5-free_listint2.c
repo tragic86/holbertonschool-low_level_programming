c7fad46d5b60024eb0f83bb3d0648c59a6d8b905
tragic86 <oneil5@aol.com>
2018-03-07 02:03:40 +0000

    free another list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 - free a linked list
+ *
+ *
+ * @head: start of list
+ *
+ * Return: none
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *temp2;
+
+	if (head == NULL)
+		return;
+
+
+	temp = *head
+	while (temp != NULL)
+	{
+		temp2 = temp->next;
+		free(temp);
+		temp = temp2;
+	}
+	*head = NULL;
+
+
+
+}
