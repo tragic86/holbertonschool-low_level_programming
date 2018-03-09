d1a1dbbb079f41cd4aadd6a21d46c2bcebbef673
tragic86 <oneil5@aol.com>
2018-03-09 04:53:12 +0000

    added header file

---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,11 @@
+#ifndef NUMBERS_H
+#define NUMBERS_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int _putchar(char c);
+#endif
