29885343f2180ec7cc724a409bc556dde7fb39b7
tragic86 <oneil5@aol.com>
2018-03-09 21:32:07 +0000

    Set value to 1 at index

---
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * set_bit - set value at given index
+ *
+ *
+ * @index: index
+ *
+ * @n: number being passed
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned long int mask = 1;
+
+	if (n == NULL)
+		return (-1);
+	if (index > 64)
+		return (-1);
+
+	mask = mask << index;
+	*n = *n | mask;
+
+	return (*n);
+
+
+
+
+
+
+
+}
