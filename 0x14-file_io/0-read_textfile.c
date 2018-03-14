a9573129119ec583403898299587fcd336adbd19
tragic86 <oneil5@aol.com>
2018-03-14 01:04:37 +0000

    print to stdout

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * read_textfile - a function that reads a text file and prints output
+ *
+ *
+ * @filename: - name of file
+ *
+ * @letters: - number of letters should read and print
+ *
+ * Return: number of letters
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	ssize_t i, k;
+	char buff[1024];
+
+
+	if (filename == NULL || letters == 0)
+		return (0);
+
+
+
+	i = open(filename, O_RDONLY | O_CREAT, S_IRUSR);
+		if (i == -1)
+			return (0);
+
+
+	k = read(i, buff, letters);
+	if (k == -1)
+		return (0);
+
+
+	write(STDOUT_FILENO, buff, k);
+	buff[k + 1] = '\0';
+
+	close(i);
+
+	return (k);
+
+}
