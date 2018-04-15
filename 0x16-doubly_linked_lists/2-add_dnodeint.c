111d73469071ce1e8a5b18526de89206d2b9ac4f
tragic86 <oneil5@aol.com>
2018-04-15 02:47:14 +0000

    moved statement to bottom

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -20,14 +20,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	*head = new_node;
 
+		
+	new_node->n = n;
 	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
 	}
+
+	
+	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
