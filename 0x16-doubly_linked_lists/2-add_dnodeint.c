88adb5c66aef42824e5a883d843df37127a122ce
tragic86 <oneil5@aol.com>
2018-04-15 02:42:46 +0000

    changed to not equal

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->next = *head;
 	*head = new_node;
 
-	if (*head == NULL)
+	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
 	}
