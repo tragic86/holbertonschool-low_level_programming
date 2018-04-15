97a0aa1e0e95a4123fd05ab0ccb4d164c963c083
tragic86 <oneil5@aol.com>
2018-04-15 02:32:44 +0000

    print list

---
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_dlistint - return number of elements in a linked list
+ *
+ * @h: node counter
+ *
+ * Return: number of elements
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	int node_counter = 0;
+	const dlistint_t *temp;
+
+	temp = h;
+	while(temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		node_counter++;
+		temp = temp->next;
+	}
+	return (node_counter);
+}
