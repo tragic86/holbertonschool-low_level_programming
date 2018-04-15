34ef198f348e6297d63e714036bd49460e146ddd
tragic86 <oneil5@aol.com>
2018-04-15 02:38:27 +0000

    add node at start

---
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+
+	dlistint_t *new_node;
+
+	if(head == NULL)
+		return (NULL);
+
+
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	if (*head == NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+
+	return (new_node);
+
+
+}
