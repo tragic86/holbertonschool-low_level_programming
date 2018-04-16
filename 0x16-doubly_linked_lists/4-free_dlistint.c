d819a3713853b87922bcc37e3a0796f9b64d3841
tragic86 <oneil5@aol.com>
2018-04-16 00:49:38 +0000

    free list

---
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->next);
+		free(temp);
+
+
+	}
+
+}
