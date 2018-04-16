c002055130236d2bf75e27d2d1c51a96fe8ab6b3
tragic86 <oneil5@aol.com>
2018-04-16 03:26:27 +0000

    betty fixes

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
 /**
  * print_dlistint - return number of elements in a linked list
  *
- * @h: node counter
+ * @h: pointer to node
  *
  * Return: number of elements
  *
@@ -18,7 +18,7 @@ size_t print_dlistint(const dlistint_t *h)
 	const dlistint_t *temp;
 
 	temp = h;
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		node_counter++;
