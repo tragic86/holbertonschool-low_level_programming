4d1a1ae2e0318add64cfbdf28cbb8123f7062011
tragic86 <oneil5@aol.com>
2018-04-16 02:09:33 +0000

    betty fixes

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,61 +1,50 @@
 #include "lists.h"
 /**
+ * insert_dnodeint_at_index - insert node at index
  *
- *
- *
- *
- *
- *
+ * @h: head
+ * @idx: index
+ * @n: data
+ * Return: address of new node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node;
 	dlistint_t *current;
-	unsigned int i;
-
+	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 	current = *h;
-
 	if (idx == 0)
 	{
-		add_dnodeint(h,n);
+		add_dnodeint(h, n);
 		return (new_node);
 	}
-
-	i = 0;
-	while(i < idx && current != NULL)
+	while (i < idx && current != NULL)
 	{
 		current = current->next;
 		i++;
 	}
-
 	if (idx != i)
 		return (NULL);
 
 	if (current == NULL)
 	{
 		add_dnodeint_end(h, n);
-		return(new_node);
+		return (new_node);
 	}
-
 	new_node->next = current;
 	new_node->prev = current->prev;
 	current->prev->next = new_node;
 	current->prev = new_node;
 
-
 	return (new_node);
-
-
-
 
 }
